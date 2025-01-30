972f0e150ae3e1c4e8a1b8f76f711e1101c24b81
projectraihan <project.raihan@proton.me>
2025-01-30 09:20:36 +0700

    update nvim config

--- .config/nvim/lua/config/opts.lua
+++ .config/nvim/lua/config/opts.lua
@@ -30,3 +30,4 @@ vim.opt.colorcolumn = "80"
 
 vim.g.netrw_browse_split = 0
 vim.g.netrw_winsize = 25
+vim.g.netrw_banner = 0
--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,4 +1,62 @@
 return {
-    'nvim-lualine/lualine.nvim',
-    options = { theme = 'base16' }
+    {
+        'nvim-lualine/lualine.nvim',
+        event = "VeryLazy",
+        opts = function()
+            -- Cache git root path
+            local git_root_cache = nil
+
+            -- Function to get the relative path from the Git root
+            local function get_relative_path()
+                local filepath = vim.fn.expand('%:p')
+                if not git_root_cache then
+                    git_root_cache = vim.fn.system('git rev-parse --show-toplevel 2>/dev/null'):gsub('%s+', '')
+                end
+                if git_root_cache ~= '' then
+                    return filepath:gsub(git_root_cache .. '/', '') -- Remove the Git root from the path
+                else
+                    return filepath                                 -- Return full path if not in a Git repo
+                end
+            end
+
+            -- Function to check for uncommitted changes using git status
+            local function get_git_status()
+                local handle = io.popen("git status --porcelain 2>/dev/null")
+
+                if not handle then
+                    return ''
+                end
+
+                local result = handle:read("*a")
+                handle:close()
+
+                -- Check if there are any changes
+                return result ~= '' and '*' or ''
+            end
+
+            -- Update git root cache on entering a buffer
+            vim.api.nvim_create_autocmd("BufEnter", {
+                callback = function()
+                    git_root_cache = nil -- Reset cache when entering a new buffer
+                end,
+            })
+
+            -- Configure lualine sections
+            return {
+                options = {
+                    theme = 'iceberg_dark',
+                    component_separators = '',
+                    section_separators = '',
+                },
+                sections = {
+                    lualine_a = { get_relative_path },
+                    lualine_b = { get_git_status },
+                    lualine_c = {},
+                    lualine_x = {},
+                    lualine_y = {},
+                    lualine_z = { '%p%%' }, -- Show percentage of file scrolled
+                },
+            }
+        end,
+    },
 }
