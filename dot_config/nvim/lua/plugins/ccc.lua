35f8fa2d48a1fbfdb788ff5690d86164fb1d98f3
project.raihan <project.raihan@proton.me>
2025-09-25 10:25:39 +0700

    update config

---
+++ dot_config/nvim/lua/plugins/ccc.lua
@@ -0,0 +1,15 @@
+return {
+    "uga-rosa/ccc.nvim",
+    config = function()
+        local ccc = require("ccc")
+        local mapping = ccc.mapping
+
+        ccc.setup({
+            highlighter = {
+                auto_enable = true,
+                lsp = true,
+            },
+        })
+    end
+
+}
